bd7002eb05087f6a1440b9273056b97c0e927fda
Raoulinet <r.piquerel@gmail.com>
2012-10-15 18:53:38 +0200

    Add a copy to clipboard function
    To improve the terminal/GUI operations.

--- usefull_tools.py
+++ usefull_tools.py
@@ -572,6 +572,149 @@ def deltaXY (timeout = 30) :
     return a[1] - a[0], b[1] - b[0]
 
 
+
+# Pyperclip v1.3
+# A cross-platform clipboard module for Python. (only handles plain text for now)
+# By Al Sweigart al@coffeeghost.net
+# Usage:
+#   import pyperclip
+#   pyperclip.copy('The text to be copied to the clipboard.')
+#   setcb("spam")
+#   spam = getcb ()
+# On Mac, this module makes use of the pbcopy and pbpaste commands, which should come with the os.
+# On Linux, this module makes use of the xclip command, which should come with the os. Otherwise run "sudo apt-get install xclip"
+
+
+import platform
+
+def winGetClipboard():
+    ctypes.windll.user32.OpenClipboard(0)
+    pcontents = ctypes.windll.user32.GetClipboardData(1) # 1 is CF_TEXT
+    data = ctypes.c_char_p(pcontents).value
+    #ctypes.windll.kernel32.GlobalUnlock(pcontents)
+    ctypes.windll.user32.CloseClipboard()
+    return data
+
+def winSetClipboard(text):
+    GMEM_DDESHARE = 0x2000
+    ctypes.windll.user32.OpenClipboard(0)
+    ctypes.windll.user32.EmptyClipboard()
+    try:
+        # works on Python 2 (bytes() only takes one argument)
+        hCd = ctypes.windll.kernel32.GlobalAlloc(GMEM_DDESHARE, len(bytes(text))+1)
+    except TypeError:
+        # works on Python 3 (bytes() requires an encoding)
+        hCd = ctypes.windll.kernel32.GlobalAlloc(GMEM_DDESHARE, len(bytes(text, 'ascii'))+1)
+    pchData = ctypes.windll.kernel32.GlobalLock(hCd)
+    try:
+        # works on Python 2 (bytes() only takes one argument)
+        ctypes.cdll.msvcrt.strcpy(ctypes.c_char_p(pchData), bytes(text))
+    except TypeError:
+        # works on Python 3 (bytes() requires an encoding)
+        ctypes.cdll.msvcrt.strcpy(ctypes.c_char_p(pchData), bytes(text, 'ascii'))
+    ctypes.windll.kernel32.GlobalUnlock(hCd)
+    ctypes.windll.user32.SetClipboardData(1,hCd)
+    ctypes.windll.user32.CloseClipboard()
+
+def macSetClipboard(text):
+    outf = os.popen('pbcopy', 'w')
+    outf.write(text)
+    outf.close()
+
+def macGetClipboard():
+    outf = os.popen('pbpaste', 'r')
+    content = outf.read()
+    outf.close()
+    return content
+
+def gtkGetClipboard():
+    return gtk.Clipboard().wait_for_text()
+
+def gtkSetClipboard(text):
+    cb = gtk.Clipboard()
+    cb.set_text(text)
+    cb.store()
+
+def qtGetClipboard():
+    return str(cb.text())
+
+def qtSetClipboard(text):
+    cb.setText(text)
+
+def xclipSetClipboard(text):
+    outf = os.popen('xclip -selection c', 'w')
+    outf.write(text)
+    outf.close()
+
+def xclipGetClipboard():
+    outf = os.popen('xclip -selection c -o', 'r')
+    content = outf.read()
+    outf.close()
+    return content
+
+def xselSetClipboard(text):
+    outf = os.popen('xsel -i', 'w')
+    outf.write(text)
+    outf.close()
+
+def xselGetClipboard():
+    outf = os.popen('xsel -o', 'r')
+    content = outf.read()
+    outf.close()
+    return content
+
+if os.name == 'nt' or platform.system() == 'Windows':
+    import ctypes
+    getcb = winGetClipboard
+    setcb = winSetClipboard
+elif os.name == 'mac' or platform.system() == 'Darwin':
+    getcb = macGetClipboard
+    setcb = macSetClipboard
+elif os.name == 'posix' or platform.system() == 'Linux':
+    xclipExists = os.system('which xclip') == 0
+    if xclipExists:
+        getcb = xclipGetClipboard
+        setcb = xclipSetClipboard
+    else:
+        xselExists = os.system('which xsel') == 0
+        if xselExists:
+            getcb = xselGetClipboard
+            setcb = xselSetClipboard
+        try:
+            import gtk
+            getcb = gtkGetClipboard
+            setcb = gtkSetClipboard
+        except:
+            try:
+                import PyQt4.QtCore
+                import PyQt4.QtGui
+                app = QApplication([])
+                cb = PyQt4.QtGui.QApplication.clipboard()
+                getcb = qtGetClipboard
+                setcb = qtSetClipboard
+            except:
+                raise Exception('Pyperclip requires the gtk or PyQt4 module installed, or the xclip command.')
+
+
+def copy_to_clipboard (s) :
+    setcb (str(s))
+    print "------- getcb -------"
+    print getcb()
+
+
+def clicked_to_clipboard () :
+    setcb(str(ginput(-1)))
+    print "------- getcb -------"
+    print getcb()
+ 
+
+def place_text (s = None) :
+    a, b = ginput(1)[0]
+    if not s :
+        s = raw_input("Text to place ? ")
+    text (a, b, s)
+
+    
 ################################################################################
 #                                                                              #
 # Different class of usefull graph.                                            #
